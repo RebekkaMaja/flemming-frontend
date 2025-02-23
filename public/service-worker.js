c5b15392ff9f077f5e1801cab11565e82c4a1b28
RebekkaMaja <10463672@ucn.dk>
2025-02-23 19:49:14 +0100

    frontend setup

---
+++ public/service-worker.js
@@ -0,0 +1,24 @@
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('pwa-cache').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/favicon.ico',
+          '/logo192.png',
+          '/manifest.json',
+        ]);
+      })
+    );
+  });
+  
+  // eslint-disable-next-line no-restricted-globals
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
